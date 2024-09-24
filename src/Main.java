5bc9e8a9a5a7274932b9c87e87be0f5b6f145f5a
Xavier Glenn <xglenn@appdev.yearup.org>
2024-09-24 13:56:03 -0700

    Our First Java Program
    Completed on 9/23, our first program!

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+//Developer: Xavier Glenn
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public static class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        String fullname = "Xavier Glenn";
+        int MyAge = 28; //int means integer
+        double weight = 193; //double should double the integer
+        System.out.println("My age is " + MyAge); //system.out.println means it'll print the line
+        System.out.println("My name is " + fullname);
+        System.out.println("My weight is " + weight);
+        System.out.println(2000 + 4000); //Java can do basic calculations
+        }
+    }
+    //Variables store information. String, int, float (decimal), char, and boolean.
+         //char gender = 'X'
+         //boolean isAnxious = 'true'
+
+String fullname = "Xavier Glenn"; //If another last name is needed, var can be overwritten.
+int mobilenumber = 111-111-1111; //a whole number. Can use -, doesn't need ""
+String email = "anemail@gmail.com";
+String password = "hahnicetry";
+
+//Code is read from top to bottom like a book would be.
+int x = 5,
+int y = 6,
+int =z =50;
+//can also use int x = 5, y = 6, x = 50;
+System.out.println(x + y + z);
+/*
+make your code readable. 'm' can be different than 'mobilenumber'.
+java is case-sensitive.
+Float needs an f after
+*/
